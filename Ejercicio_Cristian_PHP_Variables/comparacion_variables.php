e7291458c4a8953f9775c5583e30f9ca16a6456c
Cristian Stiven Ruiz Rodriguez <125284840+cristian20050103@users.noreply.github.com>
2024-03-08 19:14:10 -0500

    Add files via upload

---
+++ Ejercicio_Cristian_PHP_Variables/comparacion_variables.php
@@ -0,0 +1,10 @@
+<?php
+$num1=42;
+$num2=50;
+if($num1 > $num2){
+    echo"el numero es mayor";
+}
+else{
+    echo"el numero es menor";
+}
+?>
---
+++ Ejercicio_Cristian_PHP_Variables/sumas_de_numeros.php
@@ -0,0 +1,6 @@
+<?php
+$n1=15;
+$n2=25;
+$resultado=$n1+$n2;
+echo "el resultado de las suma de 15 + 25 es====> $resultado"
+?>
